3a5ad04b65b0fa81ff0c079c444de9d49c94bd60
musteata88 <musteataeugen2022@gmail.com>
2024-03-26 22:19:25 +0200

    update commit

--- game.js
+++ game.js
@@ -1,25 +1,37 @@
 let moldLeft = 225
 let score = 0
+let duration = 5
 const mold = document.getElementsByClassName('mold')[0]
-const egg = document.getElementsByClassName('egg')[0]
+const eggs = document.getElementsByClassName('egg')
+const scoreDiv = document.getElementsByClassName('scoreDiv')[0]
 
 // HW2:put some limits
 const moveMold = (e) => {
     switch (e.code) {
         case 'ArrowRight':
-           if (moldLeft < 450) {
-            moldLeft += 5}
+            if (moldLeft < 450) {
+                moldLeft += 5
+            }
             break
         case 'ArrowLeft':
             if (moldLeft > 0) {
-            moldLeft -= 5}
+                moldLeft -= 5
+            }
             break
     }
     mold.style.left = `${moldLeft}px`
 }
 const animationEnd = (e) => {
+    let egg = e.target
     if (e.animationName == 'move') {
-        if (125 <= moldLeft && moldLeft <= 150) {
+        if (125 <= moldLeft && moldLeft <= 150 
+            &&
+            egg.classList.contains('left') // HW4:Use className
+            ||
+            305 <= moldLeft && moldLeft <= 330 
+            &&
+            egg.classList.contains('right')//HW4:Use className
+            ) {
             score++
             // egg.classList.remove('move')
             egg.className = egg.className.replace("move", "")
@@ -28,18 +40,24 @@ const animationEnd = (e) => {
                 egg.className += ' move'
             }, 500)
             document.querySelector('.mold').innerText = score
+            duration--
+            if (duration <= 0)
+                duration = 0.5
+            egg.style.setProperty('animation-duration', `${duration}s`)
+
         } else {
             score--
             // HM3: do the same using .className
             // egg.classList.remove('move')
             // egg.classList.add('fall')
-            egg.className = egg.className.replace("move", " fall")         
+            egg.className = egg.className.replace("move", " fall")
+            egg.style.removeProperty('animation-duration')
             if (score > 0) {
-            document.querySelector('.mold').innerHTML = score
-            } else {
-                alert ('GAME OVER!')
-            }
+                document.querySelector('.mold').innerHTML = score
+            } 
         }
+
+
     }
     if (e.animationName == 'fall') {
         // egg.classList.add('egg-broken')
@@ -50,8 +68,12 @@ const animationEnd = (e) => {
             egg.classList.add('move')
         }, 1000);
     }
+    scoreDiv.innerHTML = `score: ${score}`
 
 }
 
 document.body.addEventListener('keydown', moveMold)
-egg.addEventListener('animationend', animationEnd)
+const eggElements = [...eggs]
+eggElements.forEach((egg) => {
+    egg.addEventListener('animationend', animationEnd)
+});
